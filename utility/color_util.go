73ef43c70fa9c21259fd4248dc2625e53a14d7a5
Praveen005 <praveen08h@gmail.com>
2024-06-24 00:29:03 +0530

    refactor: add ANSI ESCAPE codes in place of third-party library to color console

--- utility/color_util.go
+++ utility/color_util.go
@@ -5,70 +5,76 @@ import (
 	"os"
 
 	"github.com/civo/cli/common"
-	"github.com/gookit/color"
 )
+
+const (
+	_Green  = "\033[32m"
+	_Yellow = "\033[33m"
+	_Blue   = "\033[34m"
+	_Magenta = "\033[35m"
+	_Red    = "\033[31m"
+	_Orange = "\033[38;5;214m"
+	_Reset  = "\033[0m"
+)
+
+func Colorize(color, value string) string {
+	return fmt.Sprintf("%s%s%s", color, value, _Reset)
+}
 
 // Green is the function to convert str to green in console
 func Green(value string) string {
-	newColor := color.FgGreen.Render
-	return newColor(value)
+	return Colorize(_Green, value)
 }
 
 // Yellow is the function to convert str to yellow in console
 func Yellow(value string) string {
-	newColor := color.New(color.FgYellow).Render
-	return newColor(value)
+	return Colorize(_Yellow, value)
 }
 
 // Orange is the function to convert str to orange in console
-func Orange(value string) string {
-	newColor := color.FgYellow.Render
-	return newColor(value)
+func Orange(value string) string{
+	return Colorize(_Orange, value)
 }
 
 // Blue is the function to convert str to blue in console
 func Blue(value string) string {
-	newColor := color.New(color.FgBlue).Render
-	return newColor(value)
+	return Colorize(_Blue, value)
 }
 
 // Magenta is the function to convert str to magenta in console
 func Magenta(value string) string {
-	newColor := color.New(color.FgMagenta).Render
-	return newColor(value)
+	return Colorize(_Magenta, value)
 }
 
 // Red is the function to convert str to red in console
 func Red(value string) string {
-	newColor := color.New(color.FgRed).Render
-	return newColor(value)
+	return Colorize(_Red, value)
 }
 
 // Error is the function to handler all error in the Cli
 func Error(msg string, args ...interface{}) {
 	common.IssueMessage()
-	fmt.Fprintf(os.Stderr, "%s: %s\n", color.Red.Sprintf("Error"), fmt.Sprintf(msg, args...))
+	fmt.Fprintf(os.Stderr, "%s: %s\n", Red("Error"), fmt.Sprintf(msg, args...))
 }
 
 // Info is the function to handler all info messages in the Cli
 func Info(msg string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, "%s: %s\n", color.Blue.Sprintf("Info"), fmt.Sprintf(msg, args...))
+	fmt.Fprintf(os.Stderr, "%s: %s\n", Blue("Info"), fmt.Sprintf(msg, args...))
 }
 
 // Warning is the function to handler all warnings in the Cli
 func Warning(msg string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, "%s: %s\n", color.Yellow.Sprintf("Warning"), fmt.Sprintf(msg, args...))
+	fmt.Fprintf(os.Stderr, "%s: %s\n", Yellow("Warning"), fmt.Sprintf(msg, args...))
 }
 
 // YellowConfirm is the function to handler all delete confirm
 func YellowConfirm(msg string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, "%s: %s", color.Warn.Sprintf("Warning"), fmt.Sprintf(msg, args...))
+	fmt.Fprintf(os.Stderr, "%s: %s", Yellow("Warning"), fmt.Sprintf(msg, args...))
 }
 
 // RedConfirm is the function to handler the new version of the Cli
 func RedConfirm(msg string, args ...interface{}) {
-
-	fmt.Fprintf(os.Stderr, "%s: %s", color.Red.Sprintf("IMPORTANT"), fmt.Sprintf(msg, args...))
+	fmt.Fprintf(os.Stderr, "%s: %s", Red("IMPORTANT"), fmt.Sprintf(msg, args...))
 }
 
 // ColorStatus is to print the status of the Instance or k8s Cluster
